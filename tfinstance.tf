2f8e9320b972cfceee0b27e0d3957fb70b924aba
Emmanuel <emmanuellutula@hotmail.com>
2025-07-25 12:54:23 +0200

    Update tfinstance.tf
    without tag

--- tfinstance.tf
+++ tfinstance.tf
@@ -15,7 +15,5 @@ provider "aws" {
 resource "aws_instance" "terraform-cloud" {
   ami           = "ami-0250b8540c1cc85d4"
   instance_type = "t2.micro"
-  tags = {
-    Name = "terraform-cloud"
-  }
+ 
 }

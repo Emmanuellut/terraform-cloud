f45dac4e7a11f484729a2cd7ac6651a168355c8d
Emmanuel <emmanuellutula@hotmail.com>
2025-07-25 12:57:11 +0200

    Update tfinstance.tf
    add tags

--- tfinstance.tf
+++ tfinstance.tf
@@ -15,5 +15,8 @@ provider "aws" {
 resource "aws_instance" "terraform-cloud" {
   ami           = "ami-0250b8540c1cc85d4"
   instance_type = "t2.micro"
+tags = {
+  Name = "terraform-sentinel"
+}
  
 }
